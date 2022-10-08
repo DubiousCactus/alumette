f0442ead6ea0613deae42b027566c57bd59e97a2
Theo Morales <theo.morales.fr@gmail.com>
2022-10-08 16:52:07 +0200

    Refactor nn

--- autograd/nn.py
+++ autograd/nn.py
@@ -12,51 +12,75 @@ Neural Nets yaaay.
 
 from typing import Any, List, Tuple, Union
 
-from .engine import Value
-from .ops import F
+from .engine import Value, ReLUOp as ReLU, TanhOp as Tanh
 
 import random
 import abc
 
 
-class Neuron:
-    def __init__(self, input_dim: int, activation='relu') -> None:
-        self._synapses = [Value(random.normalvariate(0, 1)) for _ in range(input_dim)]
-        self._activation = 'relu'
+class Module:
+    def zero_grad(self):
+        for p in self.parameters():
+            p._grad = 0
 
-    def __call__(self, inputs: Union[Value,List]) -> Any:
+    def parameters(self) -> List[Value]:
+        return []
+
+
+class Neuron(Module):
+    def __init__(self, input_dim: int, bias=True, activation="relu") -> None:
+        self._synapses = [Value(random.uniform(-1, 1)) for _ in range(input_dim)]
+        self._bias = Value(random.uniform(-1, 1)) if bias else None
+        self._activation = "relu"
+
+    def __call__(self, inputs: Union[Value, List]) -> Any:
         output = Value(0)
         if isinstance(inputs, Value):
             inputs = [inputs]
-        assert isinstance(inputs, list) or isinstance(inputs, Value), "Neuron inputs should be a Value or a list!"
+        assert isinstance(inputs, list) or isinstance(
+            inputs, Value
+        ), "Neuron inputs should be a Value or a list!"
+        assert len(inputs) == len(
+            self._synapses
+        ), "Dim of inputs doesn't match dim of synapses!"
         for i, x in enumerate(inputs):
             output += self._synapses[i] * x
+        if self._bias is not None:
+            output += self._bias
         if self._activation == "relu":
             # TODO: Gradient for this? (done?)
-            output = F.ReLU(output)
+            output = ReLU.act(output)
+        elif self._activation == "tanh":
+            output = Tanh.act(output)
         elif self._activation == "identity":
             pass
         else:
-            raise NotImplementedError(f"Activation {self._activation} is not implemented")
+            raise NotImplementedError(
+                f"Activation {self._activation} is not implemented"
+            )
         return output
 
-class Layer:
-    def __init__(self, input_dim: int, output_dim: int, activation='relu') -> None:
-        self.neurons = [Neuron(input_dim, activation=activation) for _ in range(output_dim)]
-        self._bias = Value(random.normalvariate(0, 1))
+    def parameters(self) -> List[Value]:
+        return self._synapses + ([self._bias] if self._bias is not None else [])
+
+
+class Layer(Module):
+    def __init__(self, input_dim: int, output_dim: int, activation="relu") -> None:
+        self.neurons = [
+            Neuron(input_dim, activation=activation) for _ in range(output_dim)
+        ]
 
     def __call__(self, inputs: List[Neuron]) -> Any:
         outputs = []
         for n in self.neurons:
-            outputs.append(n(inputs) + self._bias)
+            outputs.append(n(inputs))
         return outputs
 
     def parameters(self) -> List[Value]:
-        return self.neurons
+        return [param for n in self.neurons for param in n.parameters()]
 
 
-
-class NeuralNet(abc.ABC):
+class NeuralNet(abc.ABC, Module):
     def __call__(self, *args: Any, **kwds: Any) -> Any:
         # TODO: Implicitely convert inputs to Values?
         assert isinstance(args[0], Value), "Input must be a Value object!"
@@ -66,39 +90,40 @@ class NeuralNet(abc.ABC):
     def forward(self, *args: Any, **kwds: Any) -> Any:
         raise NotImplementedError
 
-    def parameters(self) -> Tuple[Value]:
+    def parameters(self) -> List[Value]:
         def find_params(obj: object) -> List[Value]:
-            if isinstance(obj, Value):
-                return [obj]
-            _params = []
-            try:
-                for _, attr in obj.__dict__.items():
-                    if isinstance(attr, Value):
-                        _params.append(attr)
-                    elif isinstance(attr, list):
-                        for a in attr:
-                            _params += find_params(a)
-                    else:
-                        _params += find_params(attr)
-            except:
-                pass
-            return _params
-        return tuple(find_params(self))
+            if isinstance(obj, Layer):
+                return obj.parameters()
+            params = []
+            for _, attr in obj.__dict__.items():
+                if type(attr) is list:
+                    for e in attr:
+                        params.extend(find_params(e))
+                else:
+                    params.extend(find_params(attr))
+            return params
 
+        return find_params(self)
 
-def MSE(x, y):
+def MSE(x: Union[List[Value], Value], y: Union[List[Value], Value]):
     if isinstance(x, list) or isinstance(y, list):
-        assert type(x) is type(y), "If one argument is a list, both arguments must be a list!"
-        assert isinstance(x, Value) and isinstance(y, Value), "Arguments of MSE must be Value objects!"
-        res = [(xi-yi)**2 for xi, yi in zip(x, y)]
+        assert type(x) is type(
+            y
+        ), "If one argument is a list, both arguments must be a list!"
+        assert isinstance(x, Value) and isinstance(
+            y, Value
+        ), "Arguments of MSE must be Value objects!"
+        res = sum([(xi - yi) ** 2 for xi, yi in zip(x, y)])
     else:
-        assert isinstance(x, Value) and isinstance(y, Value), "Arguments of MSE must be Value objects!"
-        res = (x-y)**2
+        assert isinstance(x, Value) and isinstance(
+            y, Value
+        ), "Arguments of MSE must be Value objects!"
+        res = (x - y) ** 2
     return res
 
 
 class SGD:
-    def __init__(self, parameters: List, lr=1e-5) -> None:
+    def __init__(self, parameters: List, lr=1e-3) -> None:
         self._params = parameters
         self._lr = lr
 
@@ -109,4 +134,4 @@ class SGD:
 
     def zero_grad(self):
         for p in self._params:
-            p._grad = None
+            p._grad = 0
